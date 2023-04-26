5021c83340006f0849393bbecad2bd8bacde0f92
dickson <dkerobean@gmail.com>
2023-04-26 14:54:49 +0000

    auto generated order code and added order status

--- frontend/models.py
+++ frontend/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 import uuid 
 from django.contrib.auth.models import User
+import random
+import string
+from datetime import datetime
 
 
 class Profile(models.Model):
@@ -19,12 +22,46 @@ class Profile(models.Model):
     
     
 class BookDelivery(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
     item = models.CharField(max_length=200, blank=True, null=True)
     item_type = models.CharField(max_length=200, blank=True, null=True)
     pickup_location = models.CharField(max_length=250, blank=True, null=True)
     destination_location = models.CharField(max_length=250, blank=True, null=True)
     sender_contact = models.CharField(max_length=200, blank=True, null=True)
     reciever_contact = models.CharField(max_length=200, blank=True, null=True)
+    order_number = models.CharField(max_length=10, unique=True, editable=False, null=True)
+    
+    STATUS_CHOICES = [
+        ('P', 'Pending'), 
+        ('A', 'Assigned'), 
+        ('PU', 'PickedUp'), 
+        ('D', 'Delivered')
+    ]
+    order_status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='P')
+    
+    
+    def save(self, *args, **kwargs):
+        # Generate an order number if one doesn't already exist
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save(*args, **kwargs)
+
+    def generate_order_number(self):
+        # Get the current date as a string in the format "YYYYMMDD"
+        date_string = datetime.now().strftime("%Y%m%d")
+
+        # Get a random alphanumeric string of length 5
+        random_string = ''.join(random.choices(
+            string.ascii_uppercase + string.digits, k=5))
+
+        # Combine the date string and random string to form the order number
+        order_number = date_string + random_string
+
+        return order_number
+
+    
+        
     
     def __str__(self):
         return self.item
