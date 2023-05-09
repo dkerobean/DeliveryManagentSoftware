b6b19cdcba651990d42ab357ee87ee960a2fc176
dickson <dkerobean@gmail.com>
2023-05-09 16:56:38 +0000

    order page categories done and UII changes

--- user_dashboard/urls.py
+++ user_dashboard/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.homePage, name="user_home"), 
-    path('orders/<str:pk>/', views.ordersPage, name="user_order"), 
+    
+    path('orders/<str:pk>/', views.ordersPage, name="user_order"),
+    path('order/<str:pk>/', views.deleteOrder, name="delete-user-order"),
+    
+    
     path('profile/<str:pk>/', views.editProfile, name="edit-profile"),
+    
 ]
--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
-from frontend.models import Profile
+from frontend.models import Profile, BookDelivery
 from django.contrib.auth.models import User
 from django.contrib import messages
 
@@ -13,7 +13,8 @@ def homePage(request):
     deliveries = user.deliveries.all()
     
     context = {
-        'deliveries':deliveries
+        'deliveries':deliveries,
+        'user':user
     }     
     return render(request, 'user_dashboard/index.html', context)
 
@@ -45,11 +46,36 @@ def editProfile(request, pk):
 def ordersPage(request, pk):
     
     user = Profile.objects.get(id=pk)
-    deliveries = user.deliveries.all()
+    deliveries = user.deliveries.filter(is_deleted=False)
+    completed = user.deliveries.filter(order_status="Completed")
+    pending = user.deliveries.filter(order_status="Pending")
+    cancelled = user.deliveries.filter(is_deleted="True")
     
     context = {
-        'deliveries':deliveries       
+        'deliveries':deliveries,
+        'completed':completed, 
+        'pending':pending, 
+        'cancelled':cancelled, 
+        'user':user
     }
     
     
     return render(request, 'user_dashboard/orders.html', context)
+
+
+@login_required(login_url="user-login")
+def deleteOrder(request, pk):
+
+    order = BookDelivery.objects.get(order_number=pk)
+    item = order.order_number
+
+    if request.method == "POST":
+        order.delete()
+        messages.success(request, 'Deleted successfully')
+        return redirect('user_order')
+
+    context = {
+        'item': item
+    }
+
+    return render(request, 'user_dashboard/delete.html', context)
